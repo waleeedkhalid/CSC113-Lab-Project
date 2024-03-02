c4e8f8f452d8d65579e1772f02c9e8fc3697a8ef
waleeedkhalid <axd.edits@gmail.com>
2024-03-02 19:30:31 +0300

    Create Pet Class

---
+++ com/csc113/project/Pet.java
@@ -0,0 +1,40 @@
+package com.csc113.project;
+
+public abstract class Pet {
+    protected String name;
+    protected int age;
+    protected String gender;
+    protected boolean isAdopted;
+
+    public Pet(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.isAdopted = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public boolean isAdopted() {
+        return isAdopted;
+    }
+    public void setAdopted(boolean isAdopted) {
+        this.isAdopted = isAdopted;
+    }
+}

1d0d047bb8884727e42824569d344ae46ddb3efb
waleeedkhalid <axd.edits@gmail.com>
2024-03-02 19:31:00 +0300

    Create TestShelter(Main) Class

---
+++ com/csc113/project/TestShelter.java
@@ -0,0 +1,10 @@
+package com.csc113.project;
+
+public class TestShelter {
+    public static void main(String[] args) {
+
+        Parrot a = new Parrot(null, 0, null, false, null);
+
+        System.out.println("Hello world !");
+    }
+}
